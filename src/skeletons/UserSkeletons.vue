1c35d2f459d51f0330c486ad35dbfddb973cb64c
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2023-02-07 01:21:19 +0100

    Add skeletons for users list

---
+++ src/skeletons/UserSkeletons.vue
@@ -0,0 +1,52 @@
+<template>
+    <section class="skeleton-group">
+        <div class="skeleton-section">
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+
+            <div
+                class="skeleton-group__item block h-10 w-full mb-10 rounded-lg bg-gradient-to-r from-gray-200 to-gray-200/25 animate-pulse"
+            ></div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "user-skeletons",
+    props: {
+        prop: Array,
+    }
+};
+</script>
+
+<style scoped></style>
