97fdf2b0ca0d2386af830579db41e16752e9b3dd
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2023-02-07 01:22:11 +0100

    Add routing and pages

---
+++ src/pages/details/UserDetails.vue
@@ -0,0 +1,48 @@
+<template>
+        <div class="m-10 max-w-sm m-auto">
+            <div class="rounded-lg border bg-white px-4 pt-8 pb-10 shadow-lg">
+                <div class="relative mx-auto w-36 rounded-full">
+                    <img
+                        class="mx-auto h-auto w-full rounded-full"
+                        :src="person?.picture?.large"
+                        alt="large avatar"
+                    />
+                </div>
+                <h1 class="my-1 text-center text-xl font-bold leading-8 text-gray-900">
+                    {{ person.name.first }} {{ person.name.last }}
+                </h1>
+                <h3 class="font-lg text-semibold text-center leading-6 text-gray-600">
+                    Location: {{ person.location.city }}
+                </h3>
+                <p class="text-center text-sm leading-6 text-gray-500 hover:text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, placeat!
+                </p>
+                <ul
+                    class="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow"
+                >
+                    <li class="flex items-center py-3 text-sm">
+                        <span>Nationality</span>
+                        <span class="ml-auto"
+                            ><span
+                                class="rounded-full bg-green-200 py-1 px-2 text-xs font-medium text-green-700"
+                                >{{ person.nat }}</span
+                            ></span
+                        >
+                    </li>
+                    <li class="flex items-center py-3 text-sm">
+                        <span>Nickname</span>
+                        <span class="ml-auto">{{ person.login.username }}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "user-details",
+    props: { prop: Array, id: String, name: String, person: Object },
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/main/MainPage.vue
@@ -0,0 +1,66 @@
+<template>
+        <main class="user-list">
+            <div class="mx-auto max-w-screen-lg px-4 pt -8 sm:px-8">
+                <div class="flex items-center px-8 controls mb-5">
+                    <SearchInput @set-search="setSearchQuery" />
+                    <SortDropdown @set-sort="setSortQuery" />
+                </div>
+                <ProfileList
+                    :users="state.users.results"
+                    :loading="state.loading"
+                    :search-query="state.searchQuery"
+                    :sort-query="state.sortQuery"
+                    @set-users="setUsers"
+                />
+                <BasePagination @set-users="setUsers" />
+            </div>
+        </main>
+</template>
+
+<script>
+import ProfileList from "../../components/ProfileList/ProfileList.vue";
+import BasePagination from "../../components/BasePagination/BasePagination.vue";
+import SortDropdown from "../../components/SortDropdown/SortDropdown.vue";
+import SearchInput from "../../components/SearchInput/SearchInput.vue";
+import getUsers from "../../services/RandomUserService.js";
+import { RANDOM_USER_URL } from "../../constants/constants";
+
+export default {
+    name: "main-page",
+    components: {
+        ProfileList,
+        BasePagination,
+        SortDropdown,
+        SearchInput,
+    },
+    data: function () {
+        return {
+            state: { users: [], loading: false, searchQuery: '', sortQuery: '' },
+        };
+    },
+    async created() {
+        this.state.loading = true;
+        this.state.users = await getUsers(RANDOM_USER_URL);
+        this.state.loading = false;
+    },
+    methods: {
+        setUsers: function (value) {
+            this.state.loading = true;
+            this.state.users = value;
+            this.state.loading = false;
+        },
+        setSortQuery: function (query) {
+            this.state.sortQuery = query;
+        },
+        setSearchQuery: function (query) {
+            this.state.searchQuery = query;
+        },
+    },
+};
+</script>
+
+<style>
+.controls {
+    position: relative;
+}
+</style>
