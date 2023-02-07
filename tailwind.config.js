90272666b562f51973f0e9875def19df8fe27381
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2023-02-07 01:20:12 +0100

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ff5b24",
+    },
+    },
+  },
+  plugins: [],
+}
