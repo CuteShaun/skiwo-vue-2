0c13ab33716f90a2cdf7b08b8ba9aaa14f8f2d4b
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2023-02-07 01:22:34 +0100

    Finalize steps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MainPage from './pages/main/MainPage.vue';
+import UserDetails from './pages/details/UserDetails.vue';
+import App from './App'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: MainPage },
+  { path: '/user-details/:id', component: UserDetails, name: "details", props: true },
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
+Vue.config.productionTip = false;
+
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
